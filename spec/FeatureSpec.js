93baccb06c1a5f99fea61c755d3b725c50af9fc3
alwinsolanky <alwinsolanky@gmail.com>
2016-08-03 15:16:52 +0100

    User Story 3 - Stormy Weather

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -8,6 +8,10 @@
 // I want to instruct a plan to take off from
 //   an airport and confirm that it is no longer in the airport
 
+// As an air traffic controller
+// To ensure safety
+// I want to prevent takeoff when weather is stormy
+
 'use strict';
 
 describe('Feature Test:', function(){
@@ -28,6 +32,13 @@ describe('Feature Test:', function(){
     plane.land(airport)
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
+  });
+
+  it('blocks takeoff if weather is stormy', function(){
+    plane.land(airport);
+    spyOn(airport, 'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
   });
 
 });
